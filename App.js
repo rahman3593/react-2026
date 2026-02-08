ceb93b6473a5ca474294e6a7fda692e928a08868
Abdul Rahman <abdulrahmana3593@gmail.com>
2026-02-08 10:48:21 +0530

    AR: Episode 2

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", {id:"parent"},
      [React.createElement("div", {id:"child1"},
-        [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")],
+        [React.createElement("h1", {id:"h1"}, "I adsssadsam h1 tag"),React.createElement("h2", {id:"h2"}, "I am h2 tag")],
      ),React.createElement("div", {id:"child2"},
-        [React.createElement("h1", {}, "I am h1 tag"),React.createElement("h2", {}, "I am h2 tag")],
+        [React.createElement("h1", {id:"h3"}, "I am h3 tag"),React.createElement("h2", {id:"h4"}, "I am h4 tag")],
      )]
 );
 console.log(parent);
